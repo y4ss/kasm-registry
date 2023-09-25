0fd7f070a9214214193a180b5aa7c9ab220e0dac
y4ss <40444225+y4ss@users.noreply.github.com>
2023-09-25 16:09:48 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Custom Image Registry',
+    description: 'Custom Image Registry',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://y4ss.github.io/kasm-registry/',
+    contactUrl: 't',
   },
   reactStrictMode: true,
   swcMinify: true,
